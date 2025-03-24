c36c34dc29564da1439aedf81efc368e76ed24bd
swyoung <swhyoung@protonmail.com>
2025-03-24 03:24:01 +0000

    Deploying to gh-pages from @ swyoung/swyoung.github.io@c75a9951895ef85e1d84de3e069748fa16efd997 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -30,19 +30,19 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/projects/";
           },
-        },{id: "nav-talks",
-          title: "Talks",
-          description: "",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/talks/";
-          },
         },{id: "nav-cv",
           title: "cv",
           description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
+          },
+        },{id: "nav-talks",
+          title: "Talks",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/talks/";
           },
         },{id: "nav-teaching",
           title: "teaching",
